59508b7b32b520756973d2952661d1bc8265f9c2
martazhengm <MZhen-Garcia1@eep-igroup.com>
2024-05-27 10:45:38 +0200

    Correción de error accessso
    Había en otro sitio en el que estaba mal y también lo he cambiado.

--- Angular/AULA/src/app/in-memory-data.service.ts
+++ Angular/AULA/src/app/in-memory-data.service.ts
@@ -12,7 +12,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 2, nombre: 'ADE', rama: 'Ciencias Sociales y Jurídicas', duracion: '4 años', precio: '6120€ (anual)' },
       { id: 3, nombre: 'Derecho', rama: 'Ciencias Sociales y Jurídicas', duracion: '4 años', precio: '6120€ (anual)' },
       { id: 4, nombre: 'Derecho + ADE', rama: 'Ciencias Sociales y Jurídicas', duracion: '6 años', precio: '7038€ (anual)' },
-      { id: 5, nombre: 'Máster accesso al Ejercicio de la Abogacia y la Procura', rama: 'Ciencias Sociales y Jurídicas', duracion: '18 meses', precio: '7000€ (pago único)' },
+      { id: 5, nombre: 'Máster acceso al Ejercicio de la Abogacia y la Procura', rama: 'Ciencias Sociales y Jurídicas', duracion: '18 meses', precio: '7000€ (pago único)' },
       { id: 6, nombre: 'Máster Oficial en Psicología General Sanitaria', rama: 'Ciencias de la Salud', duracion: '2 años', precio: '8969€ (anual)' }
     ];
     return {carreras};
